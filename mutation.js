0f7cdb79e89f24243f752c27a0b80b66ca6fa73a
Filip Cekani <mfcekani@gmail.com>
2021-10-26 07:28:11 +0300

    someFixes

---
+++ mutation.js
@@ -0,0 +1,11 @@
+function mutation(arr) {
+const [str1, str2] = arr.map(s => s.toLowerCase());
+for (let i = 0; i < str2.length; i++) {
+    const letter = str2[i];
+    if(str1.indexOf(letter) === -1){
+        return false;
+    }
+}
+return true;
+}
+console.log(mutation("Yallo","Hallo"));
