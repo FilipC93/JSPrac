3d76abdd455910a8d4af3c0d2a2cc9316952f20f
Filip Cekani <mfcekani@gmail.com>
2021-11-14 05:45:42 +0200

    comments

--- javascript/dnaPairing.js
+++ javascript/dnaPairing.js
@@ -1,5 +1,7 @@
 function pairElement(char) {
+//Return each strand as an array of two elements, the original and the paired one.
     var paired = [];
+//Function to check with strand to pair.
     var search = function (char) {
         switch (char) {
             case "A":
@@ -16,9 +18,11 @@ function pairElement(char) {
                 break;
         }
     };
+//Loops through the input and pair.
     for (var i = 0; i < str.length; i++) {
         search(str[i]);
     }
     return paired;
 }
+//test here
 pairElement("GCG");
