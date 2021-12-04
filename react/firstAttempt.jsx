e6c6e9f5b462f7874789e55a01a5e5f4ca92203d
Filip Cekani <mfcekani@gmail.com>
2021-12-04 18:09:20 +0200

    attemptingReact

---
+++ react/firstAttempt.jsx
@@ -0,0 +1,11 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    } render() {
+        return (
+            <div>
+                <h3>YALLO React</h3>
+            </div>
+        )
+    }
+}
