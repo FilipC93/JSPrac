ed7ed1a68eb3993134b59890ce23d81b5bebce93
Filip Cekani <mfcekani@gmail.com>
2022-07-12 17:31:24 +0300

    Consise & Refactoring

--- toDo/script.js
+++ toDo/script.js
@@ -1,16 +1,15 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
-
 const todos = JSON.parse(localStorage.getItem("todos"));
 
 if (todos) {
-    todos.forEach((todo) => {
+    todos.forEach(todo => {
         addTodo(todo);
     });
 }
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", e => {
     e.preventDefault();
 
     addTodo();
@@ -30,40 +29,32 @@ function addTodo(todo) {
         }
 
         todoEl.innerText = todoText;
-
         todoEl.addEventListener("click", () => {
             todoEl.classList.toggle("completed");
 
             updateLS();
         });
 
-        todoEl.addEventListener("contextmenu", (e) => {
+        todoEl.addEventListener("contextmenu", e => {
             e.preventDefault();
-
             todoEl.remove();
-
             updateLS();
         });
-
         todosUL.appendChild(todoEl);
-
         input.value = "";
-
         updateLS();
     }
 }
 
 function updateLS() {
     const todosEl = document.querySelectorAll("li");
-
     const todos = [];
 
-    todosEl.forEach((todoEl) => {
+    todosEl.forEach(todoEl => {
         todos.push({
             text: todoEl.innerText,
             completed: todoEl.classList.contains("completed"),
         });
     });
-
     localStorage.setItem("todos", JSON.stringify(todos));
 }
