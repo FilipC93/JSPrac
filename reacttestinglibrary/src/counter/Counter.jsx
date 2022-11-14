3d75843fa206ea2a10c356c7ee137fff0d7606e3
Filip Cekani <mfcekani@gmail.com>
2022-11-14 23:48:26 +0200

    Refactor and playing with the component

--- reacttestinglibrary/src/counter/Counter.jsx
+++ reacttestinglibrary/src/counter/Counter.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 
 const Counter = () => {
     const [counterValue, setCounterValue] = useState(0);
+    const [inputValue, setInputValue] = useState(1);
 
     const incrementCounterValue = () => {
-        setCounterValue(counterValue + 1);
+        setCounterValue(counterValue + inputValue);
+        setInputValue(inputValue + 1);
     }
 
     const decrementCounterValue = () => {
@@ -14,7 +16,7 @@ const Counter = () => {
     return (
         <div>
             <h3 data-testid="header">My Counter</h3>
-            <h3 data-testid="input">1</h3>
+            <h3 data-testid="input">{inputValue}</h3>
             <button onClick={incrementCounterValue} data-testid="add-button">+</button>
             <h2 data-testid="counter">{counterValue}</h2>
             <button onClick={decrementCounterValue} data-testid="minus-button">-</button>
