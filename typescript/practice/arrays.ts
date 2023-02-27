d68143b74742c4a59820665c8b0a9f5bd0e5efb6
FilipC93 <mfcekani@gmail.com>
2023-02-27 21:00:49 +0200

    Understanding arrays in TS

---
+++ typescript/practice/arrays.ts
@@ -0,0 +1,11 @@
+const ingredients: string[] = [];
+const miligramofIngredients: number[] = [];
+
+//?Alternative way of array declaration is:
+const myArray: Array<number> = [];
+type User = { name: string, isActive: boolean, id: number }
+const allUsers: User[] = [];
+
+ingredients.push('sugar');
+miligramofIngredients.push(200);
+allUsers.push({ name: 'Filip', isActive: false, id: 135131 });
