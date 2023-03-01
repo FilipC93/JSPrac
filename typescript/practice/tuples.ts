ae63c9fa234ed0d8a64685191303258a80b263ca
FilipC93 <mfcekani@gmail.com>
2023-03-01 04:38:53 +0200

    Starting tup;les.ts

---
+++ typescript/practice/tuples.ts
@@ -0,0 +1,14 @@
+const admin: string[] = ['fc'];
+let tupledArr: [string, number, boolean];
+//*This would be on specific order only.
+tupledArr = ['fc', 93, true]
+//? Another example:
+let rgbSample: [number, number, number];
+rgbSample = [255, 43, 10];
+
+type User = [number, string];
+const newUser: User = [1939, 'exampler@example.com'];
+newUser[1] = 'fc';
+//? This is not working anymore.
+//* newUser.push(true);
+export { }
