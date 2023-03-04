8c09dbd9572beaea06c57c0481b4224f584380c9
FilipC93 <mfcekani@gmail.com>
2023-03-04 21:11:02 +0200

    Updated classes with getters and setters

--- typescript/practice/classes.ts
+++ typescript/practice/classes.ts
@@ -1,9 +1,25 @@
 class User {
+    private _courseCount = 1;
     private readonly city?: string = 'Los Angeles'; //? Can use the # for private as well.
     constructor(public email: string, public name: string) {
         this.email = email;
         this.name = name;
     }
+    private deleteToken() {
+        return 'Token deleted';
+    }
+    get getGmail(): string {
+        return `your gmail is : ${this.email}`;
+    };
+    get courseCount(): number {
+        return this._courseCount;
+    };
+    set courseCount(courseNum: number) { //? Setters dont need a return type
+        if (courseNum <= 1) {
+            throw new Error('Course count should be more than 1');
+        }
+        this._courseCount = courseNum;
+    };
 }
 
 export const chad = new User('chad@example.com', 'chad');
